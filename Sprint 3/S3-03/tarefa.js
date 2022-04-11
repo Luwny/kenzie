ce7e41458edc3fb2eecfbb00fed4d9c17f3926eb
Adam Neves <adaamangelow@gmail.com>
2022-04-11 13:48:09 -0300

    wip: new activities

---
+++ Sprint 3/S3-03/tarefa.js
@@ -0,0 +1,16 @@
+// Ativ. 1
+let valor1 = 15
+let valor2 = 876
+console.log (valor1+valor2)
+
+//Ativ. 2
+function myFunction(valor) {
+    console.log(typeof valor);
+}
+myFunction("text");
+
+//Ativ. 3
+function myFunction2(nota1, nota2, nota3, nota4){
+    console.log(nota1+nota2+nota3+nota4)
+}
+myFunction2(1, 2, 3, 4)
