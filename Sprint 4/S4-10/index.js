ae760e0c1d9ac59f32d4e72c6fafa262d1b7623c
Adam Neves <adaamangelow@gmail.com>
2022-04-25 16:04:49 -0300

    wip: new activities

---
+++ Sprint 4/S4-10/index.js
@@ -0,0 +1,10 @@
+let ganhador = {
+    nome: 'Adamastor',
+    nascimento: '16/09/1986',
+    cpf: '123.321.789-98',
+    estadoCivil: 'Casado'
+}
+function testWinner (winnerData) {
+    if (winnerData == )
+}
+testWinner(ganhador.cpf, ganhador.nome)
